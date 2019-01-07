1d2b65f5a3801045b88acd95a6801715fa477699
NadiaAiraf <aidanfaria@gmail.com>
2019-01-07 18:44:43 +0000

    rejigged things, first test to see if there's a div element

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         hello world!
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from 'react';
+
+const App = () => <div className="app-container" />;
+
+export default App;
