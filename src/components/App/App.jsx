4b53107e736d699ed65e807794ce81e0e88cefea
NadiaAiraf <aidanfaria@gmail.com>
2019-01-08 17:23:43 +0000

    set up how my calc is going to call things

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,6 +18,10 @@ import React from 'react';
 import './App.css';
 import Calculator from '../Calculator/Calculator';
 
-const App = () => <div className="app-container">this is my calculator: <Calculator /></div>;
+const App = () => (
+  <div className="app-container">
+    this is my calculator: <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Calculator = () => <div className="calculator">calc</div>;
+class Calculator extends Component {
+  state = {
+    displayValue: '0',
+    numbers: [],
+    operators: [],
+    selecterOperator: '',
+    storeValue: '',
+  }
+  
+  callOperator() {
+    console.log('call operation')
+  }
+  
+  setOperator() {
+    console.log('set operation')
+  }
+  
+  updateDisplay() {
+    console.log('update display')
+  }
+  
+  render() {
+    return(
+      <div className="calc-container">Calculator</div>
+    )
+  }
+}
 
 export default Calculator;
