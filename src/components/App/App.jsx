d6346faec725a13aece478ffcd8e7bd3d725613c
NadiaAiraf <aidanfaria@gmail.com>
2019-01-08 17:10:43 +0000

    created calculator component, app component renders calc component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,8 @@
 
 import React from 'react';
 import './App.css';
+import Calculator from '../Calculator/Calculator';
 
-const App = () => <div className="app-container">hello world</div>;
+const App = () => <div className="app-container">this is my calculator: <Calculator /></div>;
 
 export default App;
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Calculator = () => <div className="calculator">calc</div>;
+
+export default Calculator;
