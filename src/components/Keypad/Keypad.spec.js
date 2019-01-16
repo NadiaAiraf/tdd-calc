bcea7eb6752f4daeb6c7b4da529fe82e918ec882
NadiaAiraf <aidanfaria@gmail.com>
2019-01-16 11:05:02 +0000

    added numbers and operators to keypad component

--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -18,6 +18,16 @@ describe('Keypad', () => {
   });
   
   it('should render a div', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').length).toEqual(3);
+  })
+  
+  it('renders the values of the numbers', () => {
+    wrapper.setProps({numbers: ['0','1','2']});
+    expect(wrapper.find('.numbers-container').text()).toEqual('012')
+  })
+  
+  it('renders the values of the operators', () => {
+    wrapper.setProps({operators: ['+','-','*','/']})
+    expect(wrapper.find('.operators-container').text()).toEqual('+-*/')
   })
 });
