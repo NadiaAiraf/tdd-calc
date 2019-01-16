8c3ff82fa16a3992f5d629ce894bb7e2c489ed78
NadiaAiraf <aidanfaria@gmail.com>
2019-01-16 14:51:05 +0000

    fixed ui css issue

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -8,7 +8,7 @@ class Calculator extends Component {
     super(props);
     this.state = {
       displayValue: '0',
-      numbers: ['0','1','2','3','4','5','6','7','8','9'],
+      numbers: ['1','2','3','4','5','6','7','8','9','0','.','ce'],
       operators: ['/','x','-','+'],
       selecterOperator: '',
       storeValue: '',
